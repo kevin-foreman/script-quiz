25daa5ef1c7b546a8c1d54e422d570614ffa0644
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-06 08:35:15 -0600

    initial commit of the day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,47 @@
-// call back function to create an interval timer (Set Interval)
+// start quiz elements, and question and answer elements
+// use "cards" for each question so they're contained nicely
 
-var countdown = function() {
-    var counter = 30;
-    // console.log(counter);
+var startButton = document.getElementById("start-btn");
+var infoEl = document.getElementById("info");
+var questionContainer = document.getElementById("question-card");
+var questionText = document.getElementById("question-1");
+var answerText = document.getElementById("answer-a");
+var answerText = document.getElementById("answer-b");
+var answerText = document.getElementById("answer-c");
+var answerText = document.getElementById("answer-d");
 
-    // Use 'setInterval()' method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-    counter--;
-    mainEl.textConent = counter;
-     if(counter === 0){
-         clearInterval(timeInterval);
-         displayTimer();
-     }
-    }, 1000);   
-};
+
+
+// Timer starts when the Start Quiz button is clicked
+// timer starts at 60 seconds
+// timer starts when the start button is clicked
+
+
+$("#start-btn").click(function() {
+    alert("Good luck!")
+    endGame()
+var count=10;
+
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  count=count-1;
+  if (count <= -1)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     // display score in relation to current high score
+     return;
+   
+  }
+  document.getElementById("timer").innerHTML=count + " seconds remaining"; // watch for spelling
+}
+});
+
+function endGame() {
+    document.getElementById("start-btn").setAttribute("class", "hidden");
+}
+
+  // Add code to html for showing the number of seconds on the page
+
