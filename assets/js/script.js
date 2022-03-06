a3363fd2818b610ba9326f876274e2dfad7991db
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-06 10:09:59 -0600

    added functionality to print the questions and answers to the screen after start button is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@
 
 var startButton = document.getElementById('start-btn');
 var nextButton = document.getElementById('next-btn');
-var questionContainerElement = document.getElementById('question-container');
+var questionContainerEl = document.getElementById('question-container');
 var questionEl = document.getElementById('question');
 var answerButtonsEl = document.getElementById('answer-buttons');
 
@@ -12,35 +12,104 @@ var answerButtonsEl = document.getElementById('answer-buttons');
 // Timer starts when the Start Quiz button is clicked
 // timer starts at 60 seconds
 // timer starts when the start button is clicked
-
-
 $("#start-btn").click(function() {
     alert("Good luck!")
-    endGame()
-var count=10;
+    startGame();
+    var count=10;
 
-var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+var counter=setInterval(timer, 1000); // counts down by 1 second
 
 function timer()
 {
   count=count-1;
   if (count <= 0)
-  {
-     clearInterval(counter);
+    {
+    clearInterval(counter);
      //counter ended, do something here
      // display score in relation to current high score
      // figure out how to store final score in local storage
      // something like localStorage.setItem  --and-- localStorage.getItem
      // return;
-   
-  }
+
+}
   document.getElementById("timer").innerHTML=count + " seconds remaining"; // watch for spelling
 }
 });
 
-function endGame() {
-    document.getElementById("start-btn").setAttribute("class", "hidden");
-}
+// Start game function which will hide the start button by added the hide class
+// will also reveal the question container element by removing that class
+var startGame = function() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove('hide')
+    setNextQuestion()
+};
+
+
+// make a function to move through the questions after the currentQuestionIndex
+var setNextQuestion = function() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+};
+
+var resetState = function() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsEl.firstChild) {
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+};
+
+var showQuestion = function(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsEl.appendChild(button)
+    })
+};
+
+// As the user selects an answer, reveal either the next question button or the restart
+// then move on to the next question in the array
+var selectAnswer = function(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+    } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+    }
+};
+
+// Give feedback if the user selects the correct or wrong answer
+var setStatusClass = function(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+};
+
+var clearStatusClass = function(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+
+  // function endGame() {
+//     document.getElementById("start-btn").setAttribute("class", "hidden");
+// }
 
 // Add code to html for showing the number of seconds on the page
 
@@ -48,40 +117,40 @@ function endGame() {
 // Questions array, yes it's long sorry about that, a refactor might include a separate JS file to keep this functionality cleaner, and allow swapping to different question sets
 // this will have to work for now
 
-const questions = [
+var questions = [
     {
-      question: 'Consider this code. How many seconds will it take for “i” to reach 5?',
+    question: 'Consider this code. How many seconds will it take for “i” to reach 5?',
       // answer array inside question array
 
-      answers: [
+    answers: [
           // return true if the correct answer was selected
           // return false if the wrong answer is selected
         { text: '5 seconds', correct: false },
         { text: 'It will never reach 5', correct: false },
         { text: '25 seconds', correct: true },
         { text: '50 seconds', correct: false }
-      ]
+]
     },
     {
-      question: 'Which of the following classes are needed to display a “+” icon?',
-      answers: [
+    question: 'Which of the following classes are needed to display a “+” icon?',
+    answers: [
         { text: 'oi', correct: false },
         { text: 'btn', correct: false },
         { text: 'mr-2', correct: false },
         { text: 'oi-plus', correct: true }
-      ]
+]
     },
     {
-      question: 'Your website can only use fonts that came preinstalled on a client device or fonts that they installed themselves.',
-      answers: [
+    question: 'Your website can only use fonts that came preinstalled on a client device or fonts that they installed themselves.',
+    answers: [
         { text: 'True', correct: false },
         { text: 'False', correct: true }      ]
     },
     {
-      question: 'In the DOMs event object, what does its target property refer to?',
-      answers: [
+    question: 'In the DOMs event object, what does its target property refer to?',
+    answers: [
         { text: 'It refers to the HTML element that was interacted with to dispatch the event.', correct: true },
         { text: 'It refers to the HTML element we want to affect as a result of our dispatched event.', correct: false }
-      ]
+    ]
     }
-  ]
+]
